ff8112fc3149c8bd50aeaea8fed2aa1daae6f337
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-14 10:48:49 -0700

    :white_check_mak: add failing test for adding description

--- BakeryVendor.Tests/ModelTests/VendorTest.cs
+++ BakeryVendor.Tests/ModelTests/VendorTest.cs
@@ -8,14 +8,20 @@ namespace BakeryVendor.Tests
   [TestClass]
   public class VendorTests
   {
-    private static string _vendorName = "";
-    private static string _vendorDescription = "";
+    private static string _vendorName = "Delila's Bakery";
+    private static string _vendorDescription = "A Sweet place to eat";
 
     private Vendor _newVendor = new Vendor(_vendorName);
     [TestMethod]
     public void Vendor_ShouldCreateVendorWithName_String()
     {
       Assert.AreEqual(_vendorName, _newVendor.Name);
+    }
+
+    [TestMethod]
+    public void Vendor_ShouldCreateVendorWithDescription_String()
+    {
+      Assert.AreEqual(_vendorDescription, _newVendor.Description);
     }
 
   }
